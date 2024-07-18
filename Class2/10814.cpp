b5fd5d7a821c83930a6740ac9bee8f608e52a235
이서연 <dltj2541@naver.com>
2024-07-18 23:02:30 +0900

    commit

--- Class2/10814.cpp
+++ Class2/10814.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 #include <utility>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
-bool compare (pair<int, string> a, pair<int, string> b) {
-    if (a.first > b.first) {
-        a < b;
+bool compare(const pair<int, string>& a, const pair<int, string>& b) {
+    if (a.first == b.first) {
+        return a.second < b.second;
     }
-    else if (a.first == b.first) {
-        a < b;
-    }
+    return a.first < b.first;
 }
 
 int main() {
     int n = 0;
     cin >> n;
 
-    vector<pair<int, string>> v;
+    vector<pair<int, string>> v(n);
 
     for (int i = 0; i < n; i++) {
         cin >> v[i].first >> v[i].second;
