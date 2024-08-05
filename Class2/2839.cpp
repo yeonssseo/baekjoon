1d54f4a8a6f8c8eee1f0180c299773d6dd9245f4
이서연 <dltj2541@naver.com>
2024-08-06 03:16:30 +0900

    commit

--- Class2/2839.cpp
+++ Class2/2839.cpp
@@ -25,17 +25,14 @@ int main()
     else {
     int cnt = 0;
     while(n >= 0) {
-        if (n < 5) {
-            cout << -1;
-            break;
-        }
         if (n % 5 == 0) {
             cout << cnt + (n / 5);
-            break;
+            return = 0;
         }
         n = n - 3;
         cnt++;
     }
+    cout << -1;
     }
 
     return 0;
