0af8f675821508a8582a98a43d41541fa6e29166
이서연 <dltj2541@naver.com>
2024-08-06 06:05:11 +0900

    commit

--- Class2/9012.cpp
+++ Class2/9012.cpp
@@ -18,7 +18,7 @@ int main()
     int n = 0;
     cin >> n;
 
-    for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
         string line = "";
         getline(cin,line);
         bool result = true;
