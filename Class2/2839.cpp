f812bac9991759a6167da362fca75f918882e205
이서연 <dltj2541@naver.com>
2024-08-06 03:08:47 +0900

    commit

---
+++ Class2/2839.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#define endl '\n'
+using namespace std;
+
+void Init()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main()
+{
+    Init();
+
+    int n = 0;
+    cin >> n;
+
+    if (n == 3) {
+        cout << 1;
+    }
+    else if (n == 4) {
+        cout << -1;
+    }
+    
+    while(true) {
+        int cnt = 0;
+        if (n % 5 == 0) {
+            cout << cnt + n % 5;
+            break;
+        }
+        n = n - 3;
+        cnt++;
+    }
+
+    return 0;
+}
