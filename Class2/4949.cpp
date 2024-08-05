1630c1491659cde2daedc7d1ac24bf552faf238e
이서연 <dltj2541@naver.com>
2024-08-06 04:03:33 +0900

    commit

---
+++ Class2/4949.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#define endl '\n'
+using namespace std;
+
+void Init()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main()
+{
+    Init();
+
+    vector <char> ch;
+
+    char input;
+    while(cin >> input && input != '.'){
+        char a = {};
+        cin >> a;
+        ch.push_back(a);
+    }
+
+    for (int i = 0; i < ch.size(); i++) {
+        cout << ch[i];
+    }
+
+    return 0;
+}
