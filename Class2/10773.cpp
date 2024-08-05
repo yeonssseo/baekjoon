d43556bf6ac8a6d3924446d1a96f5cc6d14e8d77
이서연 <dltj2541@naver.com>
2024-08-06 07:30:19 +0900

    commit

---
+++ Class2/10773.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stack>
+#define endl '\n'
+using namespace std;
+
+void Init()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main()
+{
+    Init();
+
+    int k = 0;
+    cin >> k;
+    stack <int> st;
+
+    for(int i = 0; i < k; i++) {
+        int a = 0;
+        cin >> a;
+        if (a == 0) {
+            st.pop();
+        } else {
+            st.push(a);
+        }
+    }
+
+    int sum = 0;
+    for (int i = 0; i < st.size(); i++) {
+        sum += st.top();
+        st.pop();
+    }
+
+    cout << sum;
+
+    return 0;
+}
