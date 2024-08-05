51ca260c909e99a20673e3240af109ff6cd42071
이서연 <dltj2541@naver.com>
2024-08-06 04:35:34 +0900

    commit

--- Class2/4949.cpp
+++ Class2/4949.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #define endl '\n'
 using namespace std;
 
@@ -10,6 +11,10 @@ void Init()
     cout.tie(NULL);
 }
 
+bool findSign(const vector <char> &vec, char a){
+    find(vec.begin(), vec.end(), a) != vec.end();
+}
+
 int main()
 {
     Init();
@@ -17,15 +22,29 @@ int main()
     vector <char> ch;
 
     char input;
-    while(cin >> input && input != '.'){
+        while(cin >> input && input != '.'){
         if (input == '(' || input == ')' || input == '[' || input ==']'){
             ch.push_back(input);
         }
     }
 
+    int si1 = 0, si2 = 0, si3 = 0, si4 = 0;
+    for(int i = 0; i < ch.size(); i++){
+        if(findSign(ch, '('))si1++;
+        if(findSign(ch, ')'))si2++;
+        if(findSign(ch, '['))si3++;
+        if(findSign(ch, ']'))si4++;
+    }
+
+    if(si1 == si2 && si3 == si4) {
+        cout << "yes" << endl;
+    }
+    else cout << "no" << endl;
+
     for (int i = 0; i < ch.size(); i++) {
         cout << ch[i];
     }
+
 
     return 0;
 }
