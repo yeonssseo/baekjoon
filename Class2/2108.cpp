359307f967b634203698fb6229d45d204e55406e
이서연 <dltj2541@naver.com>
2024-08-06 15:50:46 +0900

    commit

--- Class2/2108.cpp
+++ Class2/2108.cpp
@@ -16,60 +16,63 @@ int main()
 {
     Init();
 
-    int n = 0;
-    cin >> n;
-    vector <int> v;
+    int x = 0;
+    cin >> x;
+    int arr[8001];
+
     int sum = 0;
     int index = 0;
     int max = 0;
-    int cnt[8001] = {0, };
-
-    for(int i = 0; i < n; i++){
+    vector<int> list;
+    for(int i = 0; i < x; i++){
         int num = 0;
         cin >> num;
-        v.push_back(num);
         sum += num;
 
-        if(num <= 0) index = -num;
-        else index = 4000 + num;
+        if(num <= 0) {
+            index = -num;
+        } else {
+            index = 4000 + num;
+        }
 
-        cnt[index]++;
+        arr[index]++;
 
-        if(cnt[index] > max) max = cnt[index];
+        if(arr[index] > max) {
+            max = arr[index];
+        }
+
+        list.push_back(num);
     }
-    sort(v.begin(), v.end());
 
-    int mean = round((float)sum / n);
-    if(mean == -0) mean == 0;
-    cout << mean << endl; //산술평균
+    float avg = sum / (float)x;
+    cout << round(avg) << endl;
 
-    int mid = 0;
-    if(n ==1){
-        mid = v[0];
-    } else{    
-    mid = v[n / 2];
-    }
-    cout << mid << endl; // 중앙값
+    sort(list.begin(), list.end());
+    cout << list[list.size()/2] << endl;
 
     bool flag = false;
     int result = 0;
     for(int i = -4000; i < 4001; i++){
-        if (i <= 0) index = -1;
-        else index = 4000 + i;
-
-        if(cnt[index] == max) {
+        if (i <= 0){
+            index = -i;
+        }
+        else {
+            index = 4000 + i;
+        }
+        
+        if (arr[index] == max){
             result = i;
 
-            if (flag){
+            if(flag){
                 break;
             }
             flag = true;
         }
     }
 
-    cout << result << endl; //최빈값
+    cout << result << endl;
 
-    cout << v[v.size() - 1] - v[0] << endl; //범위
+    cout << list[list.size() - 1] - list[0];
 
     return 0;
 }
