e5a64d0e84608efc1ef7666e2a7101e2131a7652
이서연 <dltj2541@naver.com>
2024-08-06 16:03:48 +0900

    commit

---
+++ Class2/1654.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#define endl '\n'
+using namespace std;
+
+void Init()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+int main()
+{
+    Init();
+
+    return 0;
+}
