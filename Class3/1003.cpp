7506247c9d83c6b6b988e9bca9c9532afbfac758
이서연 <dltj2541@naver.com>
2024-08-14 19:27:03 +0900

    commit

--- Class3/1003.cpp
+++ Class3/1003.cpp
@@ -24,14 +24,14 @@ int main()
     one[0] = 0;
     one[1] = 1;
 
-    for(int i = 0; i <= t; i++) {
+    for(int i = 0; i < t; i++) {
         int n = 0;
         cin >> n;
         for(int j = 2; j <= n; j++){
             zero[j] = zero[j - 1] + zero[j - 2];
             one[j] = one[j - 1] + one[j - 2];
         }
-        cout << zero[n] << one[n] << endl;
+        cout << zero[n] << " " << one[n] << endl;
     }
 
     return 0;
